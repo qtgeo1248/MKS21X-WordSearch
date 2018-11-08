752cc570ff82352ca227b192416ca4fd50a7a9b2
qtgeo1248 <gzhou00@stuy.edu>
2018-11-07 19:41:04 -0500

    made constructor

--- WordSearch.java
+++ WordSearch.java
@@ -1,3 +1,14 @@
 public class WordSearch {
-    
+    private char[][] data;
+
+    /**Initialize the grid to the size specified
+     *and fill all of the positions with '_'
+     *@param row is the starting height of the WordSearch
+     *@param col is the starting width of the WordSearch
+     */
+     public WordSearch(int rows, int cols) {
+         data = new char[rows][cols];
+     }
+
+     
 }
