44d849164c283b307e056de191ffaad0d18d7852
qtgeo1248 <gzhou00@stuy.edu>
2018-11-07 19:50:28 -0500

    finished toSTring and fixed the other previous methods

--- WordSearch.java
+++ WordSearch.java
@@ -4,7 +4,7 @@ public class WordSearch {
     public WordSearch(int rows, int cols) {
         data = new char[rows][cols];
         for (int m = 0; m < data.length; m++) {
-            for (int n = 0; n < data[n].length; n++) {
+            for (int n = 0; n < data[m].length; n++) {
                 data[m][n] = '_';
             }
         }
@@ -12,9 +12,20 @@ public class WordSearch {
 
     private void clear() {
         for (int m = 0; m < data.length; m++) {
-            for (int n = 0; n < data[n].length; n++) {
+            for (int n = 0; n < data[m].length; n++) {
                 data[m][n] = '_';
             }
         }
+    }
+
+    public String toString() {
+        String grid = "";
+        for (int m = 0; m < data.length; m++) {
+            for (int n = 0; n < data[m].length - 1; n++) {
+                grid += data[m][n] + " ";
+            }
+            grid += data[m][data[m].length - 1] + "\n";
+        }
+        return grid;
     }
 }
